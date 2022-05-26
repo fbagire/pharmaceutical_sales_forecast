6860580735e28e075d4b18722f06d82796e2492e
fbagire <faibagire@gmail.com>
2022-05-26 08:28:48 +0200

    Renaming data on dvc remote storage

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = 'data/train_v2.csv'
+path = 'data/train.csv'
 repo = "C:/Users/Faith Bagire/PycharmProjects/pythonProject/sales_predict"
-version = "\'v2\'"
+version = "v3"
 
 data_url = dvc.api.get_url(
     path=path,
@@ -21,4 +21,4 @@ if __name__ == '__main__':
     warnings.filterwarnings('ignore')
     np.random.seed(40)
     with mlflow.start_run(nested=True) as mlrun:
-        data = pd.read_csv(data_url)
+        data = pd.read_csv(data_url, index_col=[0])
