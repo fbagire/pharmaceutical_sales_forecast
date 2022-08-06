c86aefc7535a2dd5de4f79b97e1016299e78ba71
fbagire <faibagire@gmail.com>
2022-08-06 11:53:08 +0200

    timeseries and pcaf

--- myscripts/preprocess_data.py
+++ myscripts/preprocess_data.py
@@ -10,9 +10,21 @@ class clean_data():
 
     # fixing outliers
     def fix_outliers(self, df: pd.DataFrame, column) -> pd.DataFrame:
+
+        """" Replace the outliers with mean value This helps with the computational analysis.
+        """
+        bounds = {}
         for col in column:
-            df[col] = np.where(df[col] > df[col].quantile(0.95), df[col].mean(), df[col])
-        logger.info('successfull removing outliers')
+            q1 = df[col].quantile(0.05)  # 0.05
+            q3 = df[col].quantile(0.95)  # 0.95
+            lower_b = q1 - (1.5 * (q3 - q1))
+            upper_b = q3 + (1.5 * (q3 - q1))
+            bounds[col] = [lower_b, upper_b]
+
+        mean_use = df[col][(df[col] > bounds[col][0]) & (df[col] < bounds[col][1])].mean()
+        df[col].where((df[col] > bounds[col][0]) & (df[col] < bounds[col][1]), mean_use, inplace=True)
+
+        logger.info('successful removing outliers')
 
         return df
 
